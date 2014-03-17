ddfaa20c0b5aaeec9a857f0ba9c170f49f79870d
kakipo <kakipo@gmail.com>
2014-03-17 20:46:51 +0900

    削除機能の実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -62,7 +62,7 @@ class EventsController < ApplicationController
   def destroy
     @event.destroy
     respond_to do |format|
-      format.html { redirect_to events_url }
+      format.html { redirect_to root_path }
       format.json { head :no_content }
     end
   end
