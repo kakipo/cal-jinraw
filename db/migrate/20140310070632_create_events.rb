a05c09b92489b957657790ea38f1cdbeb32b9198
kakipo <kakipo@gmail.com>
2014-03-17 18:11:58 +0900

    seed ファイルの新規作成

--- db/migrate/20140310070632_create_events.rb
+++ db/migrate/20140310070632_create_events.rb
@@ -2,14 +2,11 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.string :url
-      t.date :event_date
       t.string :title
       t.string :place
       t.string :address
       t.integer :price
       t.integer :capacity
-      t.text :description
-
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require "csv"
+
+CSV.foreach('db/seeds.csv') do |row|
+  # url: string
+  # title: string
+  # place: string
+  # address: string
+  # price: integer
+  # capacity: integer
+  # session_id: string
+  # start_at: datetime
+  # end_at: datetime
+  # cat_beginner_flg: boolean
+  # cat_pro_flg: boolean
+  # prefecture_id: integer
+  Event.create!(
+    url: row[0],
+    title: row[1],
+    place: row[2],
+    address: row[3],
+    price: row[4].to_i,
+    capacity: row[5].to_i,
+    session_id: row[6],
+    start_at: row[7],
+    end_at: row[8],
+    cat_beginner_flg: row[9],
+    cat_pro_flg: row[10],
+    prefecture_id: row[11].to_i
+  )
+end
