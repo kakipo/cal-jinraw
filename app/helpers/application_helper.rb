5b87f210f925ffab63a0d402c2c792a3d73e0e27
kakipo <kakipo@gmail.com>
2014-03-19 17:11:46 +0900

    - イベント詳細にカテゴリ名を表示するよう修正 - カテゴリ名の取得に i18n 利用

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
 
+
+  def simple_date(dt)
+    return if dt.blank?
+    dt.strftime("%Y-%m-%d(#{%w(日 月 火 水 木 金 土)[dt.wday]}) %H:%M") 
+  end
+
   def simple_datetime(dt)
     return if dt.blank?
     # dt.strftime("%Y-%m-%d(#{%w(日 月 火 水 木 金 土)[dt.wday]})") 
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,4 +4,15 @@ module EventsHelper
     event.session_id == request.session_options[:id]
   end
 
+  def str_categories(event)
+    t_event = I18n.t("activerecord.attributes.event")
+    arr = []
+    arr << t_event[:cat_beginner_flg] if event.cat_beginner_flg
+    arr << t_event[:cat_pro_flg] if event.cat_pro_flg
+    arr << t_event[:cat_weekday_flg] if event.cat_weekday_flg
+    arr << t_event[:cat_holiday_flg] if event.cat_holiday_flg
+    arr << t_event[:cat_allnight_flg] if event.cat_allnight_flg
+    arr.join("/")
+  end
+
 end
